e20084858df0a3ab6b2e4b901991080dd0fc050c
Andriy Melnyk <melnyk.andreew@gmail.com>
2017-11-02 01:31:54 +0200

    add models

---
+++ src/main/java/ua/pp/avmelnyk/junto/models/Book.java
@@ -0,0 +1,64 @@
+package ua.pp.avmelnyk.junto.models;
+
+import java.util.List;
+
+public class Book {
+
+    private Long bookID;
+    private String name;
+    private String author;
+    private Long ISBN;
+    private String genre;
+    private List<User> owners;
+
+    public Book(String name, String author, Long ISBN, String genre) {
+        this.name = name;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.genre = genre;
+    }
+
+    public Long getBookID() {
+        return bookID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Long getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(Long ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public List<User> getOwners() {
+        return owners;
+    }
+
+    public void setOwners(List<User> owners) {
+        this.owners = owners;
+    }
+}
---
+++ src/main/java/ua/pp/avmelnyk/junto/models/User.java
@@ -0,0 +1,73 @@
+package ua.pp.avmelnyk.junto.models;
+
+import java.util.List;
+
+public class User {
+    private Long userID;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private List<Book> bookList;
+
+    public User(String username, String password, String firstName, String lastName, String email) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+}
