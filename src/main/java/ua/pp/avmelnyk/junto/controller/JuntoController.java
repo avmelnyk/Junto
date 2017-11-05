00df968d28e9b717d1eed9d2bb0d531c0a1c7e24
avmelnyk <melnyk.andreew@gmail.com>
2017-11-05 22:48:40 +0200

    add book page

--- src/main/java/ua/pp/avmelnyk/junto/controller/JuntoController.java
+++ src/main/java/ua/pp/avmelnyk/junto/controller/JuntoController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import ua.pp.avmelnyk.junto.model.Book;
 import ua.pp.avmelnyk.junto.model.User;
 
 @Controller
@@ -20,6 +21,12 @@ public class JuntoController {
     @RequestMapping(value = "/books", method = RequestMethod.GET)
     public String getBooks(){
         return"books";
+    }
+
+    @RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
+    public String getBook(@PathVariable("id")int book_id, Model model){
+        Book book;
+        return "book";
     }
 
 }
---
+++ src/main/webapp/WEB-INF/views/book.jsp
@@ -0,0 +1,29 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: avmelnyk
+  Date: 11/5/17
+  Time: 10:43 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Junto</title>
+</head>
+<body>
+    <h1> Book name </h1>
+    <h2> Author</h2>
+    <h2> ISBN </h2>
+    <h2> Genre </h2>
+    <h2> Rating </h2>
+        <ol>
+            <li>Owner</li>
+            <li>Owner</li>
+            <li>Owner</li>
+            <li>Owner</li>
+            <li>Owner</li>
+            <li>Owner</li>
+            <li>Owner</li>
+        </ol>
+</body>
+</html>
